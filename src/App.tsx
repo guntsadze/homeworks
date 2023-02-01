eb82d421db4946b029c411d9ee1ff00b351f4e7f
guntsadze <dimitri.guncadze@gmail.com>
2023-02-01 23:51:35 +0400

    first homework

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,38 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <div className="qrCardContainer">
+        <img src={"/assets/image-qr-code.png"} alt="qrImg" />
+        <h2>Improve your front-end skills by building projects</h2>
         <p>
-          Edit <code>src/App.tsx</code> and save to reload.
+          Scan the QR code to visit Frontend Mentor and take your coding skills
+          to the next level
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
+      <div className="ethCardContainer">
+        <div className="eth-clock">
+          <img src={"assets/icon-clock.svg"} />
+          <span>3 days left</span>
+          <div className="ethContainer">
+            <img src={"assets/icon-ethereum.svg"} />
+            <span>0.041 ETH</span>
+          </div>
+        </div>
+        <h3>Our Equilibrium collection promotes balance and calm.</h3>
+        <div className="rectangle"></div>
+        <div className="profile">
+          <img src={"assets/image-avatar.png"} />
+          <div className="nameUsername">
+            <span>Jules Wyvern</span>
+            <span>Creation of </span>
+          </div>
+        </div>
+        <h2>Equilibrium #3429</h2>
+        <img src={"assets/image-equilibrium.jpg"} />
+      </div>
     </div>
   );
 }
